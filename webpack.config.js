f3b9814c5965a117472aa3ae074515a2445d2501
Erk Struwe <estruwe@estruwe.de>
2018-11-25 16:36:28 +0100

    bootstrap, babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,28 @@ module.exports = {
     },
     devServer: {
         port: process.env.DEV_SERVER_PORT || 3001,
+        overlay: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
     },
 }
